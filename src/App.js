97317ffe46042cd9417a272a8ddc2eaed21ea1f0
surajverma2587 <surajverma2587@gmail.com>
2023-10-19 20:12:10 +0100

    styled form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Stack from "react-bootstrap/Stack";
 import { Banner } from "./components/Banner";
 import { AlertBanner } from "./components/AlertBanner";
 import { Result } from "./components/Result";
+import { Calculator } from "./components/Calculator";
 
 export const App = () => {
   return (
     <Container className="my-5">
       <Stack gap={3}>
         <Banner />
+        <Calculator />
         <AlertBanner message="Please complete the form!" />
         <Result />
       </Stack>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,27 @@
+import Stack from "react-bootstrap/Stack";
+import Form from "react-bootstrap/Form";
+
 export const Calculator = () => {
-  return <div>Calculator</div>;
+  return (
+    <Stack className="card p-3" gap={3}>
+      <h3 className="text-center">Calculator</h3>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="taxYear">Select Tax Year</Form.Label>
+          <Form.Select id="taxYear">
+            <option value="2021-2022">2021-2022</option>
+            <option value="2022-2023">2022-2023</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="annualSalary">Gross Annual Salary</Form.Label>
+          <Form.Control type="number" id="annualSalary" />
+          <Form.Text className="text-danger">
+            Please enter a valid annual salary.
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </Stack>
+  );
 };
